39578b75fefa4a25d8c2431995da90490e823050
daviddg10 <david.gallegos@7-eleven.com.mx>
2020-11-04 23:42:22 -0600

    se modifica port main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
